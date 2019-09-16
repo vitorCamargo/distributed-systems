5040826c7562d0c03873fae9ec6fc33f6cde5509
Vitor Bueno <vitorcamargo@alunos.utfpr.edu.br>
2019-09-16 01:40:22 -0300

    UDP Upload

--- UDP/File Upload/UDPClient.java
+++ UDP/File Upload/UDPClient.java
@@ -1,8 +1,12 @@
-package chat_udp;
+package upload_udp;
 
 import java.net.*;
 import java.io.*;
 import javax.swing.JOptionPane;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.nio.file.*;
 
 public class UDPClient {
     public static void main(String args[]) {
@@ -10,30 +14,66 @@ public class UDPClient {
         int resp = 0;
 
         try {
+            String rootFile = "C:\\Users\\vitor\\Downloads\\Telegram Desktop";
+            File fileName = new File(rootFile);
+            File[] listFiles = fileName.listFiles();
+            String[] files = getAllFiles(listFiles);
+
             dgramSocket = new DatagramSocket();
-            
-            String dstIP = JOptionPane.showInputDialog("IP Destino?");
-            int dstPort = Integer.parseInt(JOptionPane.showInputDialog("Porta Destino?"));
+
+            // String dstIP = JOptionPane.showInputDialog("IP Destino?");
+            // int dstPort = Integer.parseInt(JOptionPane.showInputDialog("Porta Destino?"));
+            String dstIP = "127.0.0.1";
+            int dstPort = 6666;
 
             InetAddress serverAddr = InetAddress.getByName(dstIP);
             int serverPort = dstPort;
 
             do {
-                String msg = JOptionPane.showInputDialog("Mensagem?");
+                String chosenFile = (String) JOptionPane.showInputDialog(null, "Escolha um arquivo para Upload", "Escolha de Arquivo", JOptionPane.QUESTION_MESSAGE, null, files, files[0]);
 
-                byte[] m = msg.getBytes();
+                File f = new File(rootFile + "\\" + chosenFile);
 
+                InputStream inStream = new FileInputStream(f);
+                ByteArrayOutputStream sendMessage = new ByteArrayOutputStream();
+                
+                sendMessage.write((byte[]) String.valueOf(f.length()).getBytes());
+                sendMessage.write((byte[]) (";" + chosenFile).getBytes());
+
+                long number = f.length()/1024 + 1;
+
+                byte[] m = sendMessage.toByteArray();
                 DatagramPacket request = new DatagramPacket(m, m.length, serverAddr, serverPort);
-
                 dgramSocket.send(request);
 
-                byte[] buffer = new byte[1000];
+                byte[] fileArray = inStream.readAllBytes();
+                inStream.close();
+
+                String md5 = calculateMD5(fileArray);
+                System.out.println(md5);
+
+                for(int i = 0; i < number; i++) {
+                    int maxContentSize = i * 1024 + 1024;
+                    if(maxContentSize > fileArray.length) maxContentSize = fileArray.length;
+
+                    byte[] fileMsg = new byte[(maxContentSize) - (i * 1024)];
+                    System.arraycopy(fileArray, i * 1024, fileMsg, 0, maxContentSize - i * 1024);
+
+                    DatagramPacket fileRequest = new DatagramPacket(fileMsg, fileMsg.length, serverAddr, serverPort);
+                    dgramSocket.send(fileRequest);
+                }
+
+                DatagramPacket checkSumRequest = new DatagramPacket(md5.getBytes(), md5.getBytes().length, serverAddr, serverPort);
+                dgramSocket.send(checkSumRequest);
+
+                byte[] buffer = new byte[1024];
                 DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
 
                 dgramSocket.receive(reply);
-                System.out.println("Resposta: " + new String(reply.getData(),0,reply.getLength()));
+                String response = new String(reply.getData(), 0, reply.getLength());
 
-                resp = JOptionPane.showConfirmDialog(null, "Nova mensagem?", "Continuar", JOptionPane.YES_NO_OPTION);
+                if(response.equals("1")) resp = JOptionPane.showConfirmDialog(null, "Arquivo enviado com Sucesso, enviar outro?", "Continuar", JOptionPane.YES_NO_OPTION);
+                else resp = JOptionPane.showConfirmDialog(null, "Falha ao enviar arquivo, enviar outro?", "Continuar", JOptionPane.YES_NO_OPTION);
             } while(resp != JOptionPane.NO_OPTION);
 
             dgramSocket.close();
@@ -42,5 +82,41 @@ public class UDPClient {
         } catch(IOException e) {
             System.out.println("IO: " + e.getMessage());
         }
-    }		      	
+    }
+
+    public static String[] getAllFiles(File[] listFiles) {
+        String[] files = new String[listFiles.length];
+        int i = 0;
+
+        for(File f: listFiles) {
+            files[i] = f.getName();
+            i++;
+        }
+
+        return files;
+    }
+
+    private static String calculateMD5(byte[] fileArray) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(fileArray);
+            byte[] digest = md.digest();
+            String myChecksum = bytesToHex(digest);
+
+            return myChecksum;
+        } catch(NoSuchAlgorithmException e) {
+            System.err.println("Could not generate MD5 hash!");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String bytesToHex(byte[] hashInBytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hashInBytes.length; i++) {
+            sb.append(Integer.toString((hashInBytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
+    }
 }
--- UDP/File Upload/UDPServer.java
+++ UDP/File Upload/UDPServer.java
@@ -1,7 +1,10 @@
-package chat_udp;
+package upload_udp;
 
 import java.net.*;
 import java.io.*;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class UDPServer {
     public static void main(String args[]) {
@@ -10,15 +13,59 @@ public class UDPServer {
             dgramSocket = new DatagramSocket(6666);
 
             while(true) {
-                byte[] buffer = new byte[1000];
+                byte[] buffer = new byte[1024];
 
                 DatagramPacket dgramPacket = new DatagramPacket(buffer, buffer.length);
                 dgramSocket.receive(dgramPacket);
 
-                System.out.println("Cliente: " + new String(dgramPacket.getData(), 0, dgramPacket.getLength()));    
-                DatagramPacket reply = new DatagramPacket(dgramPacket.getData(), dgramPacket.getLength(), dgramPacket.getAddress(), dgramPacket.getPort());
+                String data = new String(dgramPacket.getData(), 0, dgramPacket.getLength());
+                String[] parts = data.split(";", 2);
+                String fileName = parts[1];
+                long fileLength = Long.parseLong(parts[0]);
 
-                dgramSocket.send(reply);
+                long number = fileLength/1024 + 1;
+
+                try {
+                    DataOutputStream outStream = new DataOutputStream(new FileOutputStream(new String(fileName)));
+
+                    byte[] bf = new byte[1024];
+                    for(int i = 0; i < number; i++) {
+                        DatagramPacket dgramFilePacket = new DatagramPacket(bf, bf.length);
+                        dgramSocket.receive(dgramFilePacket);
+
+                        byte[] recv = new byte[dgramFilePacket.getLength()];
+                        System.arraycopy(bf, 0, recv, 0, dgramFilePacket.getLength());
+
+                        outStream.write(recv);
+                        outStream.flush();
+                    }
+                    outStream.close();
+
+                    byte[] checksum = new byte[1024];
+                    DatagramPacket dgramCheckSumPacket = new DatagramPacket(checksum, checksum.length);
+                    dgramSocket.receive(dgramCheckSumPacket);
+
+                    InputStream inStream = new FileInputStream(new File(fileName));
+                    String oldMD5 = new String(dgramCheckSumPacket.getData(), 0, dgramCheckSumPacket.getLength());
+                    String md5 = calculateMD5(inStream.readAllBytes());
+                    inStream.close();
+
+                    System.out.println(oldMD5);
+                    System.out.println(md5);
+
+                    if(oldMD5.equals(md5)) {
+                        DatagramPacket reply = new DatagramPacket("1".getBytes(), "1".getBytes().length, dgramPacket.getAddress(), dgramPacket.getPort());
+                        dgramSocket.send(reply);
+                    } else {
+                        DatagramPacket reply = new DatagramPacket("2".getBytes(), "2".getBytes().length, dgramPacket.getAddress(), dgramPacket.getPort());
+                        dgramSocket.send(reply);
+                    }
+                } catch(IOException e) {
+                    e.printStackTrace();
+
+                    DatagramPacket reply = new DatagramPacket("2".getBytes(), "2".getBytes().length, dgramPacket.getAddress(), dgramPacket.getPort());
+                    dgramSocket.send(reply);
+                }
             }
         } catch(SocketException e) {
             System.out.println("Socket: " + e.getMessage());
@@ -27,5 +74,29 @@ public class UDPServer {
         } finally {
             dgramSocket.close();
         }
+    }
+
+    private static String calculateMD5(byte[] fileArray) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(fileArray);
+            byte[] digest = md.digest();
+            String myChecksum = bytesToHex(digest);
+
+            return myChecksum;
+        } catch(NoSuchAlgorithmException e) {
+            System.err.println("Could not generate MD5 hash!");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String bytesToHex(byte[] hashInBytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hashInBytes.length; i++) {
+            sb.append(Integer.toString((hashInBytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
     }
 }
